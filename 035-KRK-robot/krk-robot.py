9b611114335456adaac36ea2e8fae505ec685e47
Christer Nilsson <janchrister.nilsson@gmail.com>
2024-09-13 18:52:57 +0200

    x

--- 035-KRK-robot/krk-robot.py
+++ 035-KRK-robot/krk-robot.py
@@ -1,4 +1,8 @@
+import random
+
 EMPTY = '.'
+
+dump = print
 
 def manhattan(p,q):
     px,py = unpack(p)
@@ -10,6 +14,21 @@ def manhattan(p,q):
 def unpack(index): return (index//8,index%8)
 def pack(x,y): return (8 * x + y)
 
+def moveHor(bky,wky,wry):
+    if bky==0: return 1
+    if bky >= 6: return bky-1
+    # if bky<wry: return bky+1
+    if bky+1 == wky: return bky-1
+    return bky+1
+
+def moveRookHor(wry):
+    if wry <= 3: return 7
+    return 0
+
+def tempodrag(wry):
+    if wry <= 3: return wry+1
+    return wry-1
+
 class ChessGame:
     def __init__(self,position):
         self.board = [EMPTY] * 64
@@ -20,7 +39,7 @@ class ChessGame:
         self.board[self.wk] = 'K'
         self.board[self.wr] = 'R'
         self.board[self.bk] = 'k'
-        self.antal = 0
+        self.antal = 1
 
     def display_board(self):
         s = ''
@@ -52,8 +71,8 @@ class ChessGame:
         wkx, wky = unpack(self.wk)
         wrx, wry = unpack(self.wr)
 
-        # Om svarta kungen är vid brädets kant och instängd av vit kung och torn, är det schackmatt
-        if (bkx in [0, 7] or bky in [0, 7]) and (abs(bkx - wkx) == 2) and (abs(bky - wky) <= 1):
+        # Om svarta kungen är vid brädets övre kant och instängd av vit kung och torn, är det schackmatt
+        if bkx == 0 and wkx == 2 and (abs(bky - wky) <= 1):
             if bkx == wrx or bky == wry:  return True
         return False
 
@@ -64,15 +83,17 @@ class ChessGame:
         wrx, wry = unpack(self.wr)
 
         # Flytta mot kanten, grundlogik för att simulera flykten
-        if bkx < 7 and bkx + 1 != wrx and abs(bkx-wkx) > 2:
+        if bkx == 0:
+            bky = moveHor(bky,wky,wry)
+        elif bkx < 7 and bkx + 1 != wrx and abs(bkx-wkx) > 2:
             bkx += 1
         elif bkx + 1 != wrx:
-            bky += 1 if bky < 7 else -1
+            bkx -= 1
+            if bky<wry: bky += 1
+            else: bky -= 1
         else:
-            if bkx == 0:
-                bky += 1 if bky < 7 else -1
-            else:
-                bkx -= 1
+            bky = moveHor(bky,wky,wry)
+
         self.move_piece('k', pack(bkx, bky))
 
     def apply_torres_algorithm(self):
@@ -84,38 +105,38 @@ class ChessGame:
         # Kontrollera om kungen och tornet är för nära varandra
         if manhattan(self.bk,self.wr) <= 2:
             if wrx in [0, 7]:
-                print("B Om tornet redan är på a- eller h-filen, gör inget")
+                pass
+                dump("B Om tornet redan är på a- eller h-filen, gör inget")
             else:
-                print("C Flytta tornet i säkerhet")
-                new_file = 0 if wrx <= 3 else 7
-                if new_file == wry: new_file = 7 - new_file
-                self.move_piece('R', pack(wrx, new_file))
+                dump("C Flytta tornet i säkerhet")
+                wry = moveRookHor(wry)
+                self.move_piece('R', pack(wrx, wry))
         else:
             dx = abs(bkx - wrx)
             if dx > 1:
-                print("E Tornet flyttas vertikalt mot svarta kungen")
+                dump("E Tornet flyttas vertikalt mot svarta kungen")
                 wrx = wrx - dx+1 if wrx > bkx else wrx + dx
                 self.move_piece('R', pack(wrx, wry))
             elif abs(bkx - wkx) > 2:
-                print("F Vita kungen flyttas vertikalt mot svarta kungen")
+                dump("F Vita kungen flyttas vertikalt mot svarta kungen")
                 wkx = (wkx - 1 if wkx > bkx else wkx + 1)
                 self.move_piece('K', pack(wkx, wky))
             elif abs(wkx - bkx) == 2:
-                # "G Kontrollera om den horisontella distansen är noll, jämn eller udda")
-                dy = abs(wky - bky)
-                if dy == 0:
+                mh = manhattan(self.wk, self.bk)
+                if wky == bky:
+                    dump("G Avancera med tornet")
                     wrx = wrx - 1
                     self.move_piece('R', pack(wrx, wry))
-                elif dy % 2 == 1:
-                    print("H Tempodrag")
-                    wry = 1 if wry == 0 else 6
+                elif mh == 3:
+                    dump("H Tempodrag")
+                    wry = tempodrag(wry)
                     self.move_piece('R', pack(wrx, wry))
                 else:
-                    print("I Flytta vita kungen horisontellt mot svarta kungen")
+                    dump("I Flytta vita kungen horisontellt mot svarta kungen")
                     wky = wky - 1 if wky > bky else wky + 1
                     self.move_piece('K', pack(wkx, wky))
             else:
-                print("J Tornet flyttas vertikalt mot svarta kungen")
+                dump("J Tornet flyttas vertikalt mot svarta kungen")
                 wrx = wrx - 1 if wrx > bkx else wrx + 1
                 self.move_piece('R', pack(wrx, wry))
 
@@ -124,20 +145,41 @@ class ChessGame:
         while not self.is_checkmate():
             self.display_board()
             if self.antal == 100: break
-            print("White",self.antal)
+            dump('Drag:',self.antal)
             self.antal += 1
             self.apply_torres_algorithm()
 
             if self.is_checkmate(): break
-            self.display_board()
-
-            print("Black",self.antal)
-            self.antal += 1
             self.move_black_king()
 
         self.display_board()
-        print("Schackmatt!")
+        #print("Schackmatt!")
 
-# game = ChessGame("d1h1d8") # KRk
-game = ChessGame("a1b2c3") # KRk
+def slump():
+    arr = []
+    for col in "abcdefgh":
+        for row in "12345678":
+            arr.append(col+row)
+    return random.choice(arr) + random.choice(arr) + random.choice(arr)
+
+# while True:
+#     position = slump()
+#     if position[1] < position[3] < position[5]:
+#         game = ChessGame(position) # KRk
+#         game.play()
+#         if game.antal >= 53:
+#             print(position,game.antal)
+
+# e7c1d5 efter rotation
+# a1b2c3 48 drag (32 i teorin)
+# e1d3f5 29 drag
+# d2f3h4 37 drag
+# b4a5h7 15 drag
+# c2d6a7 14 drag
+game = ChessGame("h1d2g3") # 53 drag
+#game = ChessGame("a1e2b3") # 49 drag
+
+#game = ChessGame("d1h1d8") # KRk
+#game = ChessGame("a1b2c3") # KRk
+#game = ChessGame("h1a3b4") # KRk
 game.play()
