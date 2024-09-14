618c18ffb07a929a11b8f3fff5881efa25f6723f
Christer Nilsson <janchrister.nilsson@gmail.com>
2024-09-14 09:34:51 +0200

    x

--- 035-KRK-robot/krk-robot.py
+++ 035-KRK-robot/krk-robot.py
@@ -1,7 +1,5 @@
 import random
-
 EMPTY = '.'
-
 dump = print
 
 def manhattan(p,q): # Square,Square
@@ -33,6 +31,8 @@ class Square:
         res = pack(_.x,_.y)
         return res
 
+def sq(square): return "abcdefgh"[square.x] + "12345678"[7 - square.y]
+
 class ChessGame:
     def __init__(_,position):
         _.board = [EMPTY] * 64
@@ -47,6 +47,9 @@ class ChessGame:
 
         _.antal = 1
 
+    # def position(_): return "abcdefgh"[_.wk.x] + "12345678"[7-_.wk.y] + "abcdefgh"[_.wr.x] + "12345678"[7-_.wr.y] + "abcdefgh"[_.bk.x] + "12345678"[7-_.bk.y]
+    def position(_): return sq(_.wk) + sq(_.wr) + sq(_.bk)
+
     def display_board(_):
         s = ''
         for row in range(8):
@@ -54,22 +57,15 @@ class ChessGame:
             for col in range(8):
                 t += '   ' + _.board[pack(col,row)]
             s += t + "\n"
-        print(s)
+        return s
 
     def move_piece(_, piece, x,y):
         """Flytta en pjäs till en ny position"""
-        if piece == 'K':
-            _.board[_.wk.index()] = EMPTY
-            _.wk = Square(x,y)
-            _.board[_.wk.index()] = 'K'
-        elif piece == 'R':
-            _.board[_.wr.index()] = EMPTY
-            _.wr = Square(x,y)
-            _.board[_.wr.index()] = 'R'
-        elif piece == 'k':
-            _.board[_.bk.index()] = EMPTY
-            _.bk = Square(x,y)
-            _.board[_.bk.index()] = 'k'
+        letter = _.board[piece.index()]
+        _.board[piece.index()] = EMPTY
+        piece.x = x
+        piece.y = y
+        _.board[piece.index()] = letter
 
     def is_checkmate(_):
         # Om svarta kungen är vid brädets övre kant och instängd av vit kung och torn, är det schackmatt
@@ -93,59 +89,59 @@ class ChessGame:
         else:
             x = moveHor(_.bk,_.wk,_.wr)
 
-        _.move_piece('k', x,y)
+        _.move_piece(_.bk, x,y)
 
     def apply_torres_algorithm(_):
         """Implementera Torres' slutspelsalgoritm baserat på de specificerade reglerna"""
 
         # Kontrollera om kungen och tornet är för nära varandra
         if manhattan(_.bk,_.wr) <= 2:
-            if _.wr.x in [0, 7]:
-                dump("B Om tornet redan är på a- eller h-filen, gör inget")
-            else:
-                dump("C Flytta tornet i säkerhet")
-                x = moveRookHor(_.wr)
-                y = _.wr.y
-                _.move_piece('R', x, y)
+            # if _.wr.x in [0, 7]:
+            #     dump("B Om tornet redan är på a- eller h-filen, gör inget")
+            # else:
+            dump("C Flytta tornet i säkerhet")
+            x = moveRookHor(_.wr)
+            y = _.wr.y
+            _.move_piece(_.wr, x, y)
         else:
             dy = abs(_.bk.y - _.wr.y)
-            if dy > 1:
+            if dy > 1 and abs(_.bk.x - _.wr.x) > 1:
                 dump("E Tornet flyttas vertikalt mot svarta kungen")
                 x = _.wr.x
                 y = _.wr.y - dy+1 if _.wr.y > _.bk.y else _.wr.y + dy
-                _.move_piece('R', x,y)
+                _.move_piece(_.wr, x,y)
             elif abs(_.bk.y - _.wk.y) > 2:
                 dump("F Vita kungen flyttas vertikalt mot svarta kungen")
                 x = _.wk.x
                 y = (_.wk.y - 1 if _.wk.y > _.bk.y else _.wk.y + 1)
-                _.move_piece('K', x,y)
+                _.move_piece(_.wk, x,y)
             elif abs(_.wk.y - _.bk.y) == 2:
                 mh = manhattan(_.wk, _.bk)
                 if _.wk.x == _.bk.x:
                     dump("G Avancera med tornet")
                     x = _.wr.x
                     y = _.wr.y - 1
-                    _.move_piece('R', x, y)
+                    _.move_piece(_.wr, x, y)
                 elif mh == 3:
                     dump("H Tempodrag")
                     x = tempodrag(_.wr)
                     y = _.wr.y
-                    _.move_piece('R', x,y)
+                    _.move_piece(_.wr, x,y)
                 else:
                     dump("I Flytta vita kungen horisontellt mot svarta kungen")
                     x = _.wk.x - 1 if _.wk.x > _.bk.x else _.wk.x + 1
                     y = _.wk.y
-                    _.move_piece('K', x,y)
+                    _.move_piece(_.wk, x,y)
             else:
                 dump("J Tornet flyttas vertikalt mot svarta kungen")
                 x = _.wr.x
                 y = _.wr.y - 1 if _.wr.y > _.bk.y else _.wr.y + 1
-                _.move_piece('R', x,y)
+                _.move_piece(_.wr, x,y)
 
     def play(_):
 
         while not _.is_checkmate():
-            _.display_board()
+            print(_.display_board())
             if _.antal == 100: break
             dump('Drag:',_.antal)
             _.antal += 1
@@ -154,7 +150,7 @@ class ChessGame:
             if _.is_checkmate(): break
             _.move_black_king()
 
-        _.display_board()
+        print(_.display_board())
         print("Schackmatt!")
 
 def slump():
@@ -173,15 +169,31 @@ def slump():
 #             print(position,game.antal)
 
 # e7c1d5 efter rotation
-# a1b2c3 48 drag (32 i teorin)
-# e1d3f5 29 drag
-# d2f3h4 37 drag
-# b4a5h7 15 drag
-# c2d6a7 14 drag
-game = ChessGame("h1d2g3") # 53 drag
-#game = ChessGame("a1e2b3") # 49 drag
 
-#game = ChessGame("d1h1d8") # KRk
-#game = ChessGame("a1b2c3") # KRk
-#game = ChessGame("h1a3b4") # KRk
-game.play()
+#  29 drag
+# d2f3h4 37 drag
+#game = ChessGame("h1d2g3") # 52 drag
+#game = ChessGame("a1e2b3") # 49 drag
+#game = ChessGame("a1b2c3") # 48 drag
+#game = ChessGame("h1a3b4") # 41 drag
+#game = ChessGame("e1d3f5") # 29 drag
+# game = ChessGame("e1d3e8") #
+#game = ChessGame("e1d3e5") #
+#game = ChessGame("b4a5h7") # 15 drag
+#game = ChessGame("c2d6a7") # 14 drag
+#game = ChessGame("d1h1d8") # 11 drag
+
+# game.play()
+
+def check(a,b,description):
+    game = ChessGame(a)
+    before = game.display_board()
+    game.apply_torres_algorithm()
+    after  = game.display_board()
+    b = game.position()
+    if a==b: return
+    print('assert failure:',description)
+    print(before)
+    print(after)
+
+check('e1d3e8','e1d6e8',"Tornet bör avancera")
